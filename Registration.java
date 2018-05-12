bec000ce7c8a495d2282b28946f999800ef39830
adavi <adavi@DESKTOP-7POBC0M>
2018-05-11 21:26:58 -0400

    new class added

---
+++ Registration.java
@@ -0,0 +1,10 @@
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 WebDriver driver = new ChromeDriver();
+        // This is Github example
+	}
+
+}
